4339d5ac5b9965f8a65a04be38ce65aacceb7ad6
Anthony Wong <awong0811@gmail.com>
2024-12-01 16:03:14 -0800

    finished mfcc tests

--- train.py
+++ train.py
@@ -5,11 +5,7 @@ import pandas as pd
 #Format input data
 def train(data, model: xgboost.XGBClassifier):
     train_feat_df, y_train, test_clean_feat_df, y_test_clean, test_noisy_feat_df, y_test_noisy = data
-    #Edit this variable to create a list that contains your feature names
-    #you could just pass in the matrix of features to xgboost
-    #but it looks prettier in the shap explainer if you format it
-    #as a dataframe.
-
+    
     model.fit(train_feat_df,y_train)
 
     print("Train Clean Acc =", np.sum(y_train==model.predict(train_feat_df))/len(y_train))
